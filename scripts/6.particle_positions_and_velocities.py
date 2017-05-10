43029717519d5fc6f72b140289afaad1ca1f43bc
abatten <adam.batten@students.mq.edu.au>
2017-05-10 12:26:29 +1000

    Added 6.particles_and_velocities.py

---
+++ scripts/6.particle_positions_and_velocities.py
@@ -0,0 +1,156 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from yt.mods import *
+
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import os
+import sys
+import ConfigParser
+
+import modules.cefunctions as cef
+
+mylog.disabled = True
+
+
+#################################################################################
+def read_inlist(ipath):
+    print(" ")
+    print("<-------------->")
+    print("READING INLIST FILE")
+    print("<-------------->")
+    inlist_name = ipath.split('/')[-1]
+    config = ConfigParser.ConfigParser()
+    config.readfp(open(inlist_name, 'r'))
+
+    # Read in the config file
+    root_dir = config.get('Common Section', 'root_dir')
+    exclude_dir = config.get('Common Section', 'exclude_dir')
+    plot_dir = config.get('Common Section', 'plot_dir')
+    initial_path = config.getint('Common Section', 'initial_path')
+    final_path_plus_one = config.getint("Common Section", "final_path_plus_one")
+    output_file_name = config.get("Position-Velocity Section", "output_file_name")
+    particle_number = config.getint("Position-Velocity Section", "particle_number")
+
+    print("INLIST FILE: " + inlist_name)
+    print("ROOT DIRECTORY: " + str(root_dir))
+    print("EXCLUDING DIRECTORIES: " + str(exclude_dir))
+    print("OUTPUT DIRECTORY: " + str(plot_dir))
+    print("INITIAL PATH: " + str(initial_path))
+    print("FINAL PATH PLUS ONE: " + str(final_path_plus_one))
+    print("OUTPUT FILE NAME: " + str(output_file_name))
+    print("PARTICLE NUMBER: " + str(particle_number))
+
+    return (root_dir, exclude_dir, plot_dir, initial_path,
+            final_path_plus_one, output_file_name, particle_number)
+
+
+def open_file(file_name, num_particles):
+    output_file = open(file_name, 'w' )
+    header = "Time (yr), Cycle(#)"
+
+    # Create header based on the number of particles
+    pos_dict = {}
+    posvec = ["x", "y", "z"]
+    vel_dict = {}
+    velvec = ["vx", "vy", "vz"]
+    for i in range(num_particles):
+        for j in range(3):
+            pos_dict[str(j+1)] = "%s_%s_%s" % ("Particle", str(i+1), posvec[j])
+            vel_dict[str(j+1)] = "%s_%s_%s" % ("Particle", str(i+1), velvec[j])
+
+            header = ", ".join([header, pos_dict[str(j+1)], vel_dict[str(j+1)]])
+
+    # Write the first line of information in the file
+    output_file.write(header + "\n")
+    return output_file
+
+def positions_velocities(directory, index, outfile):
+    str_index = cef.index2str(index)
+
+    print(" ")
+    print("<-------------->")
+    print("READ ROOTDIRECTORY " + str_index + ":", directory)
+    print("<-------------->")
+    pf = load(directory)
+
+    #gets the length, time and mass units used in the current simulation    
+    length_unit1 = pf.parameters['LengthUnits']
+    time_unit1 = pf.parameters['TimeUnits']
+    mass_unit1 = pf.parameters['MassUnits']
+
+    current_cycle = pf.parameters['InitialCycleNumber']
+    current_time = pf.current_time
+
+    #adds the whole data as an object called common_envelope. It is an array and you can acces the data by knowing their name through: common_envelope["Dataname"]:
+    common_envelope = pf.h.all_data()
+
+    if (index == initial_path):
+        current_cycle = 0
+    else:
+        current_cycle = pf.parameters['InitialCycleNumber']
+
+    particle_masses = common_envelope['ParticleMassMsun']
+    # The core of the primary has the largest mass
+    primary_mass = np.max(particle_masses)
+
+    for i in range(len(particle_masses)):
+        if particle_masses[i] == primary_mass:
+            primary_index = i
+            break
+        else:
+            pass
+    print("Primary Index: ", primary_index)
+
+    primary_coords = [common_envelope['particle_position_x'][primary_index] * length_unit1,
+                      common_envelope['particle_position_y'][primary_index] * length_unit1,
+                      common_envelope['particle_position_z'][primary_index] * length_unit1]
+
+    primary_vel = [common_envelope['particle_velocity_x'][primary_index] * length_unit1,
+                   common_envelope['particle_velocity_y'][primary_index] * length_unit1,
+                   common_envelope['particle_velocity_z'][primary_index] * length_unit1]
+
+
+    particle_indicies = common_envelope['particle_index']
+    print(particle_indicies)
+    print(common_envelope['particle_position_x'][particle_indicies] * length_unit1,
+                                  common_envelope['particle_position_y'][particle_indicies] * length_unit1,
+                                  common_envelope['particle_position_z'][particle_indicies] * length_unit1)
+       
+
+    output_file.write(str(current_time)+" "+str(current_cycle)+" "+str(primary_coords[0])+" "
+                     +str(primary_vel[0])+" "+str(primary_coords[1])+" "+str(primary_vel[1])+" "
+                     +str(primary_coords[2])+" "+str(primary_vel[2]))
+
+    
+    for i in range(len(particle_indicies)):
+        if i != primary_index:
+            output_file.write(str(common_envelope["particle_position_x"][particle_indicies[i]])+" "
+                              +str(common_envelope["particle_velocity_x"][particle_indicies[i]])+" " 
+                              +str(common_envelope["particle_position_y"][particle_indicies[i]])+" "
+                              +str(common_envelope["particle_velocity_y"][particle_indicies[i]])+" "
+                              +str(common_envelope["particle_position_z"][particle_indicies[i]])+" "
+                              +str(common_envelope["particle_velocity_z"][particle_indicies[i]]))
+
+    outfile.write("\n")
+         
+
+if __name__ == "__main__":
+
+    (root_dir, exclude_dir, plot_dir, initial_path,
+     final_path_plus_one, output_file_name, particle_number) = read_inlist(sys.argv[1])
+
+
+    # Sort the root directory
+    root_dir_list = cef.root_sort(root_dir, exclude=exclude_dir)
+
+    # Set output file name and open it to write
+    output_file_name = plot_dir + output_file_name
+    output_file = open_file(output_file_name, particle_number)
+    
+
+    for index in range(initial_path, final_path_plus_one):
+        positions_velocities(root_dir_list[index], index, output_file)
+    output_file.close()
+
---
+++ scripts/modules/constants.py
@@ -0,0 +1,33 @@
+xMo = 1.9891E+33 # solar mass 
+xLo = 3.839E+33  # solar luminosity
+xRo = 6.9551E+10 # solar radii
+xAU = 1.4960E+13 # Astro unit
+
+xY  = 3.1558E+07 # 1 year
+xD  = 86400      # 1 day
+xHour  = 3600    # 1 hour
+
+xG  = 6.67428E-08 # Gravitational constant
+xR  = 8.3145E+07  # Gas constant
+xA  = 7.5657E-15  # Radiation constant
+
+xclight = 29979245800 # Speed of light
+
+xNa = 6.0221e23 # Avogadro number mol-1
+
+xK = 1.380658E-16 # Boltzmann constant in erg.K-1
+xE0 = 13.6 # energy to ionized h in eV
+xA0 = 5.291772083E-9 # Bohr radius in cm
+xMe = 9.109382E-28 # Electron mass in g
+xMH = 1.673532499E-24 # Mass of hydrogen atom in g
+xh = 6.62606876E-34 # Planck constants in J.s
+xSB = 5.6705E-5 # Stefan-Boltzmann constant
+
+xEV = 1.602176462E-19 # 1Ev in joules
+xJ = 1E7 # 1J in ergs
+
+xMJ = 1.8986E+30; # Jupiter mass
+xRJ = 7.1492E+9;  # Jupiter radius
+xME = 5.9722E+27; # Earth mass
+xRE = 6.371E+8;   # Earth radius
+
