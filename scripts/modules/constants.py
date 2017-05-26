d864900cc6c8949e8c1195658327bca6bed27f8e
abatten <adam.batten@students.mq.edu.au>
2017-05-26 10:30:14 +1000

    Added more constants. Made all constants uppercase.

--- scripts/modules/constants.py
+++ scripts/modules/constants.py
@@ -1,33 +1,101 @@
-xMo = 1.9891E+33 # solar mass 
-xLo = 3.839E+33  # solar luminosity
-xRo = 6.9551E+10 # solar radii
-xAU = 1.4960E+13 # Astro unit
+"""
+This file is a list of physical constants
 
-xY  = 3.1558E+07 # 1 year
-xD  = 86400      # 1 day
-xHour  = 3600    # 1 hour
+PREFIXES:
 
-xG  = 6.67428E-08 # Gravitational constant
-xR  = 8.3145E+07  # Gas constant
-xA  = 7.5657E-15  # Radiation constant
+X indicates the unit is in SI units.
+Y indicates the unit is in cgs units
+"""
+XYR = 31557600          # Year (s)
+XDAY = 86400            # Day (s)
+XHR = 3600              # Hour (s)
 
-xclight = 29979245800 # Speed of light
+XGRAM = 0.001           # Gram (kg)
+YKG = 1000              # Kilogram (g)
 
-xNa = 6.0221e23 # Avogadro number mol-1
+XMSUN = 1.9886e30       # Solar Mass (kg)
+YMSUN = 1.9886e33       # Solar Mass (g)
+XRSUN = 6.957e8         # Solar Radius (m)
+YRSUN = 6.957e11        # Solar Radius (cm)
+XLSUN = 3.839e26        # Solar Luminosity (W)
+YLSUN = 3.839e33        # Solar Luminosity (ergs/s)
+XTSUN = 5780            # Solar Temperature (K)
+XVSUN = 1.41e27         # Solar Volume (m^3)
+YVSUN = 1.41e36         # Solar Volume (cm^3)
 
-xK = 1.380658E-16 # Boltzmann constant in erg.K-1
-xE0 = 13.6 # energy to ionized h in eV
-xA0 = 5.291772083E-9 # Bohr radius in cm
-xMe = 9.109382E-28 # Electron mass in g
-xMH = 1.673532499E-24 # Mass of hydrogen atom in g
-xh = 6.62606876E-34 # Planck constants in J.s
-xSB = 5.6705E-5 # Stefan-Boltzmann constant
+XMEARTH = 5.972e24      # Earth Mass (kg)
+YMEARTH = 5.972e27      # Earth Mass (g)
+XREARTH = 6.371e6       # Earth Radius (m)
+YREARTH = 6.371e8       # Earth Radius (cm)
+XGEARTH = 9.8067        # Earth Gravitational Acceleration (m/s^2)
 
-xEV = 1.602176462E-19 # 1Ev in joules
-xJ = 1E7 # 1J in ergs
+XMJUP = 1.898e27        # Jupiter Mass (kg)
+YMJUP = 1.898e30        # Jupiter Mass (g)
+XRJUP = 7.1492e7        # Jupiter Radius (m)
+YRJUP = 7.1492e9        # Jupiter Radius (cm)
 
-xMJ = 1.8986E+30; # Jupiter mass
-xRJ = 7.1492E+9;  # Jupiter radius
-xME = 5.9722E+27; # Earth mass
-xRE = 6.371E+8;   # Earth radius
+XMMOON = 7.348e22       # Moon Mass (kg)
+YMMOON = 7.348e25       # Moon Mass (g)
+XRMOON = 1.738e6        # Moon Radius (m)
+YRMOON = 1.738e8        # Moon Radius (cm)
+XDMOON = 3.84402e8      # Moon-Earth Distance (m)
+YDMOON = 3.84402e10     # Moon-Earth Distance (cm)
 
+XMELE = 9.109382e-31    # Electron Mass (kg)
+YMELE = 9.109382e-28    # Electron Mass (g)
+
+XMHYD = 1.6735325e-27   # Mass of Hydrogen Atom (kg)
+YMHYD = 1.6735325e-24   # Mass of Hydrogen Atom (g)
+
+XKM = 1.0e3             # Kilometer (m)
+YKM = 1.0e4             # Kilometer (cm)
+XCM = 0.01              # Centimeter (m)
+YM = 100                # Meter (cm)
+
+XLY = 9.461e15          # Light year (m)
+YLY = 9.461e18          # Light year (cm)
+
+XAU = 1.4960e11         # Astronomical Unit (m)
+YAU = 1.4960e13         # Astronomical Unit (cm)
+
+XPC = 3.08567758128e16  # Parsec (m)
+YPC = 3.08567758128e18  # Parsec (cm)
+
+YJ = 1.0e7              # Joule (ergs)
+XERG = 1.0e-7           # erg (J)
+
+XDYNE = 1.0e-5          # Dyne (N)
+YNEWT = 1.0e5           # Newton (dyne)
+
+XG = 6.67408e-11        # Gravitational Constant (m^3/kg/s^2)
+YG = 6.67408e-8         # Gravitational constant (cm^3/g/s^2)
+
+XH = 6.62607e-34        # Planks Constant (J.s)
+YH = 6.62607e-27        # Planks Constant (ergs.s)
+XHBAR = 1.0546e-34      # Reduced Planks Constant (J.s)
+YHBAR = 1.0546e-27      # Reduced Planks Constant (ergs.s)
+
+XAMU = 1.6605390e-27    # Atomic Mass Unit (kg)
+YAMU = 1.6605390e-24    # Atomic Mass Unit (g)
+
+XCLIGHT = 299792458     # Speed of light in a vaccum (m/s)
+YCLIGHT = 29979245800   # Speed of light in a vacuum (cm/s)
+
+XEV = 1.602176462e-19   # Electron Volt (J)
+YEV = 1.602176462e-12   # Electron Volt (ergs)
+
+XK = 1.380658e-23       # Boltzmann Constant (J/K)
+YK = 1.380658e-16       # Boltzmann Constant (erg/K)
+
+XNA = 6.0221e23         # Avagadros Number (mol^-1)
+
+XR = 8.3145             # Gas Constant (J/mol/K)
+YR = 8.3145e7           # Gas Constant (ergs/mol/K)
+
+XSB = 5.67e-8           # Stefan-Boltzmann Constant (W/m^2/K^4)
+YSB = 5.67e-5           # Stefan-Boltzmann Constant (erg/s/cm^2/K^4)
+
+XRBOHR = 5.29177e-11    # Bohr Radius (m)
+YRBOHR = 5.29177e-9     # Bohr Radius (cm)
+
+XFSC = 7.297353e-3      # Fine Structure Constant (no units)
