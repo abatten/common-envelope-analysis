894bb8b0fc43f8938296a100ac80a5477f887db9
abatten <adam.batten@students.mq.edu.au>
2017-05-29 09:32:24 +1000

    Fixed the issue with run.sh whre it would skip directories and overwrite the input list.

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,19 +3,23 @@
 
 # The first argument is the script you want to run
 SCRIPT=$1
-
+INLIST=inlist_ce_analysis_template3.txt
 # Range depends on number of directories in path.
-for i in {0..142}
+FINAL_PATH=5
+for i in $(seq 0 ${FINAL_PATH})
 do
     echo $i
+    
+    ANALYSIS_FILE=$(mktemp ce_analysis.XXXXX)
     # Read the inlist file and change the variables
-    cat inlist_ce_analysis_template2.txt |
+    cat $INLIST |
     sed "s/INITIAL_PATH/$i/" |
     sed "s/FINAL_PATH_PLUS_ONE/$(($i+1))/" |
-    sed "s/OUTPUT_FILE_NAME/$i/" > inlist_ce_analysis_template2.txt
+    sed "s/OUTPUT_FILE_NAME/$i/" > "$ANALYSIS_FILE"
 
     # Run the script for each directory at the same time.
     # /dev/null 2>&1 causes no output to be displayed.
-    python $SCRIPT inlist_ce_analysis_template2.txt & #> /dev/null 2>&1 &
+    python $SCRIPT "$ANALYSIS_FILE" > /dev/null 2>&1 &
+    rm $ANALYSIS_FILE
 done
 wait
