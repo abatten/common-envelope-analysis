e23369c1caf70f511f25d06ea165483c6815cc55
abatten <adam.batten@students.mq.edu.au>
2017-07-24 10:55:37 +1000

    Added more comments

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 
+# This script is used to run multiple instances of the same
+# Common Envelope python script to help speed up the analysis.
+
 # The first argument is the script you want to run
 SCRIPT=$1
+
+# This is the inlist of parameters that will be modified by this script.
 INLIST=inlist_ce_analysis_template.ini
 
-# If the script is the energy script
-if [[ $SCRIPT == *"energy"* ]]; then
-    echo " "
-    echo "<------------>"
-    echo "Running Energy Script"
-    echo "<------------>"
-    echo " "
-    OUTPUT_NAMES=$"energy_components_"
-    echo "Output File Names:" $OUTPUT_NAMES
-fi
-
 # Range depends on number of directories in path.
-FINAL_PATH=5
+# The FINAL_PATH is equal to the total number of directories.
+FINAL_PATH=136
+
 for i in $(seq 0 ${FINAL_PATH})
 do
     echo $i
-    # Make Temp file to use as an inlist
+    # Create a temp inlist for the python script to read
     ANALYSIS_FILE=$(mktemp ce_analysis.XXXXX)
+
     # Read the inlist file and change the variables
     cat $INLIST |
     sed "s/INITIAL_PATH/$i/" |
@@ -31,8 +28,8 @@ do
     # Run the script for each directory at the same time.
     # /dev/null 2>&1 causes no output to be displayed.
     python $SCRIPT "$ANALYSIS_FILE" > /dev/null 2>&1 &
-    rm $ANALYSIS_FILE
 done
 wait
 
-
+# Remove all the temp files after the script finishes.
+rm ce_analysis.*
